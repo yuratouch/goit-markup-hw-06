ffd441f4244d65181f39de77f4c4f9bcdac402db
yuratouch <yu.touch@icloud.com>
2024-09-29 23:03:00 +0300

    Final updates

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,19 +1,19 @@
 (() => {
     const refs = {
       // Додати атрибут data-modal-open на кнопку відкриття
-      openModalBtn: document.querySelector('[data-modal-open]'),
+      openMenuBtn: document.querySelector('[data-menu-open]'),
       // Додати атрибут data-modal-close на кнопку закриття
-      closeModalBtn: document.querySelector('[data-modal-close]'),
+      closeMenuBtn: document.querySelector('[data-menu-close]'),
       // Додати атрибут data-modal на бекдроп модалки
-      modal: document.querySelector('[data-modal]'),
+      menu: document.querySelector('[data-menu]'),
     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
   
-    function toggleModal() {
+    function toggleMenu() {
       // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-      refs.modal.classList.toggle('is-open');
+      refs.menu.classList.toggle('is-open');
     }
     
   })();
